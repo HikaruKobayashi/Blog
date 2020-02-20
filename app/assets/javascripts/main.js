e92b89a0cf38756843c422bc6633bac53e5f38d8
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-02-20 11:25:02 +0900

    UIの修正と管理者ではない場合の対処。

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", function() {
+  let scroll = window.pageYOffset;
+
+  // スクロール率で背景色を変化させる。
+  if (scroll > 6000) {
+    document.body.style.backgroundColor = "rgb(242,237,228)";
+  }  else if (scroll > 4500) {
+    document.body.style.backgroundColor = "rgb(238,234,234)";
+  } else if (scroll > 3000) {
+    document.body.style.backgroundColor = "rgb(236,238,232)";
+  } else if (scroll > 1500) {
+    document.body.style.backgroundColor = "rgb(232,238,232)";
+  } else {
+    document.body.style.backgroundColor = "rgb(232,238,238)";
+  }  
+});
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,3 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
-  is_impressionable
 end
