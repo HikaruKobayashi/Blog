b9a8e655abe0edb80baa5a127e54bf44340c11a1
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-07-16 08:22:54 +0900

    テストにまつわる設定をする。

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,13 @@ module Myapp
 
     # precompileを失敗させない。
     config.assets.initialize_on_precompile = false
+
+    # テストケースについて
+    config.generators do |g|
+      g.test_framework :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
