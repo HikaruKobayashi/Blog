a7fa640649cab8fc0e74bf9e2c34afa7934fdcad
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-07-16 15:42:39 +0900

    モデルスペックを書き直す。

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before do 
-    @user = build(:user)
-  end
-
   describe 'バリデーション' do
-    it 'nameとemailどちらも値が設定されていれば、OK' do
+    it 'nameとemailどちらも値が設定されていれば成功' do
+      user = FactoryBot.build(:user)
       expect(@user.valid?).to eq(true)
     end
 
-    it 'nameが空だとNG' do
-      @user.name = ''
-      expect(@user.valid?).to eq(false)
+    it 'nameが空だと失敗' do
+      user = FactoryBot.build(:user, name: nil)
+      user.valid?
+      expect(user.errors[:name]).to include("can't be blank")
     end
 
-    it 'emailが空だとNG' do
-      @user.email = ''
-      expect(@user.valid?).to eq(false)
+    it 'emailが空だと失敗' do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
     end
   end
 end
